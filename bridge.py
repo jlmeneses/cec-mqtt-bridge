6676f443453e7ee9a4222df304217f3eacf72382
jlmeneses <jorge@meneses.com.au>
2020-01-04 20:40:57 +1100

    stop tv.service

--- bridge.py
+++ bridge.py
@@ -332,7 +332,7 @@ try:
                 raise Exception("Could not connect to cec adapter")
         except Exception as e:
             print("ERROR: Could not initialise CEC:", str(e))
-            os.service('tvservice -o')
+            os.system('tvservice -o')
             exit(1)
 
     ### Setup IR ###
